94c7cf6756227d7348f49ef8f5230ed14a6f5a01
Lexi Tran <hazukitran@gmail.com>
2015-11-01 12:32:52 +0700

    Restructuring

--- oo_tictactoe.rb
+++ oo_tictactoe.rb
@@ -1,26 +1,12 @@
-# Draw a board
-# Human = 'X'
-# Computer = "O"
-# check empty_position?
-# check board_full?
-# render board
-# check_winner
-
 require 'pry'
-
 class Board
 
-  WINNING_LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
-
-  attr_accessor :board
+  attr_accessor :board, :marker
 
   def initialize
     @board = {}
     (1..9).each { |position| @board[position] = " " }
-  end
-
-  def available_positions
-    board.select { |key, value| value == " "}.keys
+    @value = marker
   end
 
   def draw
@@ -37,67 +23,73 @@ class Board
     puts "  #{@board[7]}  |  #{@board[8]}  |  #{@board[9]}  "
     puts "     |     |   "
   end 
+   
+  def available_positions
+    board.select { |key, value| value == " "}.keys
+  end
 
   def board_full?
     available_positions.size == 0
   end
 
-  def check_winner
-    WINNING_LINES.each do |pattern|
-      return "Player" if board.values_at(*pattern).count("X") == 3
-      return "Computer" if board.values_at(*pattern).count("O") == 3
-    end
-    nil
-  end
-
 end
 
-class Player
+class Human
 
-  attr_accessor :board, :position
+  attr_accessor :name, :marker
 
   def initialize
-    @position = position 
-    @board = Board.new
+    @name = name
+    @marker = marker
   end
 
-end
-
-class Human < Player
-
-  attr_accessor :name
-
-  def get_name
+  def name
     puts "Enter your name: "
     gets.chomp
   end
 
-  def human_marker
+  def marker
     puts "Pick any character except 'O': "
     gets.chomp
   end
 
-  def place_move
-    begin
-      puts "Pick a position: #{board.available_positions}"
-      self.position = gets.chomp.to_i
-    end until board.available_positions.include?(self.position)
-      board[position] = human_marker
-  end
-
 end
 
-class Computer < Player
+class Computer
 
-  attr_accessor :human, :name
+  attr_accessor :name, :position, :marker
+  
+  def initialize
+    @name = "Mr.Robot"
+    @position = position
+    @marker = "O"
+  end
+  
+end
 
-  def initialize(marker)
+class TicTacToe
+
+  WINNING_LINES = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]]
+
+  attr_accessor :human, :board, :computer
+
+  def initialize
     @human = Human.new
-    @name = "Mr.Computer"
-  end 
+    @computer = Computer.new
+    @board = Board.new
+    @current_player = @human
+  end
+
+  def check_winner
+    WINNING_LINES.each do |pattern|
+      return "#{human.name}" if board.values_at(*pattern).count("X") == 3
+      return "#{computer.name}" if board.values_at(*pattern).count("O") == 3
+    end
+    nil
+  end
 
   def winning_moves
-    Board::WINNING_LINES.each do |pattern|
+    TicTacToe::WINNING_LINES.each do |pattern|
       if board.values_at(*pattern).count(symbol) == 2 && board.values_at(*pattern).count(" ") == 1
         pattern.each do |position|
           if board[position] == " "
@@ -109,62 +101,56 @@ class Computer < Player
     false
   end
 
-  def place_move
-    unless winning_moves(board, "O") || winning_moves(board, human.human_marker)
-      position = available_positions(board).sample
-      board[position] = "O"
+  def computer_move
+    unless winning_moves(board, computer.marker) || winning_moves(board, human.marker)
+      position = board.available_positions.sample
+      board[position] = computer.marker
     end
   end
 
-end
-
-# game engine
-class Game
-
-  attr_accessor :human, :computer, :board
-
-  def initialize
-    @human = Human.new
-    @computer = Computer("O")
-    @board = Board.new
-    @current_player = @human
+  def human_move
+    begin
+      puts "Pick a position: #{board.available_positions}"
+      position = gets.chomp.to_i
+    end until board.available_positions.include?(position)
+    board[position] = human.marker
   end
 
   def alternate_player
-    if @current_player == human
-      @current_player = computer
+    if @current_player == @human
+      human_move
     else
-      @current_player = human
+      computer_move
     end
   end
 
   def play_again?
-    puts "Play again? 'n' to quit"
+    puts "Play again? 'n' to quit "
     gets.chomp.downcase == 'n'
-
-  def play
-    begin
-      loop do
-        board.draw
-        @current_player.place_move
-
-        if @current_player.(board.check_winner)
-          puts "#{current_player.name} wins."
-          break
-        elsif board.board_full?
-          puts "It's a draw."
-          break
-        else
-          alternate_player
-        end
-      end
-    
-      board.draw
-      break if play_again?
-      initialize  
-    end
   end
 
+  def play
+    board.draw
+    loop do
+      alternate_player
+      board.draw
+
+      if check_winner
+        puts "The winner is #{@current_player.name}!"
+        break
+      elsif board.board_full?
+        puts "It's a draw."
+        break
+      else
+        alternate_player
+      end
+
+      break if play_again?
+
+    end
+  end
 end
 
-Game.new.play
+TicTacToe.new.play
+
+
